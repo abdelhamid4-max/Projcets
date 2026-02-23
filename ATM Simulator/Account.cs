c302abadbfea43d144805c7fa93db7224b4952e0
Abdelhamid <abdelhamida982@gmail.com>
2026-02-23 19:45:39 +0200

    first commit

---
+++ ATM Simulator/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_Simulator
+{
+    internal class Account
+    {
+        decimal balance=0;
+        internal void Deposit()
+        {
+            Console.WriteLine("Enter The Amount To Deposit");
+            bool deposit = decimal.TryParse(Console.ReadLine(), out decimal result_deposit);
+            
+                balance += result_deposit;
+                Console.WriteLine("The Deposit secceded");
+            
+        }
+        internal void Withdraw()
+        {
+            Console.WriteLine("Enter The Amount Want To Withdraw");
+            bool withdraw = decimal.TryParse(Console.ReadLine(), out decimal result_Withdraw);
+            if (result_Withdraw > balance)
+            {
+                Console.WriteLine("The Amount Less Than Of Withdraw");
+            }
+            else
+            {
+
+                balance = balance - result_Withdraw;
+                Console.WriteLine("The Withdraw secceded");
+
+            }
+            }
+        
+        internal void CheckBalance()
+        {
+            Console.WriteLine($"Your Balance = {balance}");
+        }
+    }
+}
---
+++ ATM Simulator/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ATM_Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice = "";
+            Account account = new Account();
+
+            while (choice!="4")
+            {
+                Console.WriteLine("---------Menu--------");
+                Console.WriteLine("1- Deposit");
+                Console.WriteLine("2- Withdraw");
+                Console.WriteLine("3- CheckBalance");
+                Console.WriteLine("4- Exit");
+                Console.WriteLine("---------------------");
+                choice =Console.ReadLine();
+                switch (choice)
+                {
+                     
+                    case "1":
+                       
+                        account.Deposit();
+                        break;
+                    case "2":
+                        account.Withdraw();
+                        break;
+                    case "3":
+                        account.CheckBalance();
+                        break;
+                    case "4":
+                        Console.WriteLine("Exit");
+                        break;
+                    default:
+                        Console.WriteLine("The Choice Not Found! Plase Select From Menu");
+                        break;
+
+
+                }
+            }
+        }
+    }
+}
